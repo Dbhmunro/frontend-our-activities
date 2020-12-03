fd9536bfc75b45afd29e5d77b3cc9dc605fabc77
dbhmunro <dbhmunro@gmail.com>
2020-12-02 21:23:23 -0500

    converted controlled user login form to use react hook form

--- src/components/UserLoginForm.js
+++ src/components/UserLoginForm.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { fetchUserLogin } from '../actions/users'
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function UserLoginForm(props) {
     // state = {
@@ -9,46 +11,48 @@ function UserLoginForm(props) {
     //     password: ""
     // }
 
-    const [form, setValues] = useState({
-        email: "",
-        password: ""
-    })
+    // const [form, setValues] = useState({
+    //     email: "",
+    //     password: ""
+    // })
 
-    const handleChange = e => {
-        // this.setState({
-        //     [e.target.name]: e.target.value
-        // })
-        setValues({
-            [e.target.name]: e.target.value
-        })
-    }
+    // const handleChange = e => {
+    //     // this.setState({
+    //     //     [e.target.name]: e.target.value
+    //     // })
+    //     setValues({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        props.dispatchFetchUserLogin(form)
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        // e.preventDefault()
+        props.dispatchFetchUserLogin(data)
         // this.setState({
         //     email: "",
         //     password: ""
         // })
-        setValues({
-            email: "",
-            password: ""
-        })
+        // setValues({
+        //     email: "",
+        //     password: ""
+        // })
     }
 
-    const errorMessage = () => {
-        if(props.message) {
-            return props.message
-        }
-    }
+    // const errorMessage = () => {
+    //     if(props.message) {
+    //         return props.message
+    //     }
+    // }
     
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="email" placeholder="Enter your email" value={form.email} onChange={handleChange} />
-            <input type="password" name="password" placeholder="Enter your password" value={form.password} onChange={handleChange} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" name="email" ref={register} placeholder="Enter your email" />
+            <input type="password" name="password" ref={register} placeholder="Enter your password" />
             <input type="submit" value="Login" />
-            <p>{errorMessage()}</p>
+            {/* <p>{errorMessage()}</p> */}
         </form>
         <h4>Don't have an account, sign up <Link to="/signup">here</Link></h4>
         </>
